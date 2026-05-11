c88e8ab15703860f82e57032fcf316cd0ce18cb3
Per Lindgren <per.lindgren@ltu.se>
2026-05-11 16:22:11 +0200

    works

--- examples/waveform.rs
+++ examples/waveform.rs
@@ -1,6 +1,11 @@
-use bevy::{color::palettes::css::*, math::Isometry2d, prelude::*};
+use bevy::{
+    color::palettes::css::*,
+    dev_tools::fps_overlay::{FpsOverlayConfig, FpsOverlayPlugin},
+    // math::Isometry2d,
+    prelude::*,
+};
 use cpal::traits::StreamTrait;
-use std::f32::consts::{FRAC_PI_2, PI, TAU};
+// use std::f32::consts::{FRAC_PI_2, PI, TAU};
 mod common;
 use common::audio_in::parse_input;
 use std::sync::{Arc, Mutex};
@@ -22,6 +27,9 @@ impl<const N: usize> WaveBuffer<N> {
     // Wraps around when the buffer is full.
     fn add_samples(&mut self, input: &[f32]) {
         for &sample in input.iter() {
+            if self.index == 0 {
+                println!(".");
+            }
             self.samples[self.index] = sample;
             //  println!("sample: {:.2} at index {}", sample, self.index);
             self.index = (self.index + 1) % self.samples.len();
@@ -29,25 +37,25 @@ impl<const N: usize> WaveBuffer<N> {
     }
 }
 
-impl Curve<Vec2> for WaveBuffer<1024> {
+impl<const N: usize> Curve<Vec2> for WaveBuffer<N> {
     // Required methods
     fn domain(&self) -> Interval {
         Interval::EVERYWHERE
     }
 
     fn sample_unchecked(&self, t: f32) -> Vec2 {
-        let index = ((t.fract() + 1.0) % 1.0 * self.samples.len() as f32) as usize;
+        let index = t as usize;
         let y = self.samples[index] * 200.0;
-        // println!("Sampling at t={:.2}: index={}, y={:.2}", t, index, y);
+        println!("Sampling at t={:.2}: index={}, y={:.2}", t, index, y);
         Vec2::new(t, y)
     }
 }
 
 #[derive(Resource)]
-struct WaveForm(Arc<Mutex<WaveBuffer<1024>>>);
+struct WaveForm<const N: usize>(Arc<Mutex<WaveBuffer<N>>>);
 
 fn main() -> Result<(), anyhow::Error> {
-    let wave_buffer = Arc::new(Mutex::new(WaveBuffer::<1024>::new()));
+    let wave_buffer = Arc::new(Mutex::new(WaveBuffer::<48000>::new()));
 
     let wave_buffer_clone = wave_buffer.clone();
     let write_input_data = {
@@ -64,10 +72,15 @@ fn main() -> Result<(), anyhow::Error> {
     stream.play()?;
 
     App::new()
-        .add_plugins(DefaultPlugins)
-        .insert_resource(WaveForm(wave_buffer))
+        .add_plugins((
+            DefaultPlugins,
+            FpsOverlayPlugin {
+                config: FpsOverlayConfig::default(),
+            },
+        ))
+        .insert_resource(WaveForm::<48000>(wave_buffer))
         .add_systems(Startup, setup)
-        .add_systems(Update, (draw_waveform,))
+        .add_systems(Update, (draw_waveform::<48000>,))
         .run();
     Ok(())
 }
@@ -76,18 +89,20 @@ fn setup(mut commands: Commands) {
     commands.spawn(Camera2d);
 }
 
-fn draw_waveform(mut gizmos: Gizmos, time: Res<Time>, wave_buffer: Res<WaveForm>) {
-    let domain = Interval::EVERYWHERE;
-    let curve = FunctionCurve::new(domain, |t| Vec2::new(t, ops::sin(t / 25.0) * 100.0));
-    let resolution = ((ops::sin(time.elapsed_secs()) + 1.0) * 50.0) as usize;
-
+fn draw_waveform<const N: usize>(
+    mut gizmos: Gizmos,
+    // _time: Res<Time>,
+    wave_buffer: Res<WaveForm<N>>,
+) {
     let wave_buffer = &*wave_buffer.0.lock().unwrap();
 
-    gizmos.curve_2d(
-        &*wave_buffer,
-        (0..=wave_buffer.samples.len())
-            .map(|n| n as f32 / wave_buffer.samples.len() as f32)
-            .map(|t| (t - 0.5) * 600.0),
+    gizmos.linestrip_2d(
+        (0..N).map(|n| {
+            let t = n as f32 / wave_buffer.samples.len() as f32;
+            let x = (t - 0.5) * 600.0;
+            let y = wave_buffer.samples[n] * 200.0;
+            Vec2::new(x, y)
+        }),
         WHITE,
     );
 }
