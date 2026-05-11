310f6d55b78b59f18030c340586ed2b3ba4a1ee6
Per Lindgren <per.lindgren@ltu.se>
2026-05-11 16:57:08 +0200

    tuner

--- examples/waveform.rs
+++ examples/waveform.rs
@@ -10,15 +10,17 @@ mod common;
 use common::audio_in::parse_input;
 use std::sync::{Arc, Mutex};
 
-struct WaveBuffer<const N: usize> {
-    samples: [f32; N],
+struct WaveBuffer {
+    len: usize,
+    samples: Vec<f32>,
     index: usize,
 }
 
-impl<const N: usize> WaveBuffer<N> {
-    fn new() -> Self {
+impl WaveBuffer {
+    fn new(len: usize) -> Self {
         Self {
-            samples: [0.0; N],
+            len,
+            samples: vec![0.0; len],
             index: 0,
         }
     }
@@ -27,9 +29,9 @@ impl<const N: usize> WaveBuffer<N> {
     // Wraps around when the buffer is full.
     fn add_samples(&mut self, input: &[f32]) {
         for &sample in input.iter() {
-            if self.index == 0 {
-                println!(".");
-            }
+            // if self.index == 0 {
+            //     println!(".");
+            // }
             self.samples[self.index] = sample;
             //  println!("sample: {:.2} at index {}", sample, self.index);
             self.index = (self.index + 1) % self.samples.len();
@@ -37,25 +39,32 @@ impl<const N: usize> WaveBuffer<N> {
     }
 }
 
-const SAMPLE_SIZE: usize = 1000;
 #[derive(Resource)]
-struct WaveForm<const N: usize>(Arc<Mutex<WaveBuffer<N>>>);
+struct WaveForm(Arc<Mutex<WaveBuffer>>);
 
 fn main() -> Result<(), anyhow::Error> {
-    let wave_buffer = Arc::new(Mutex::new(WaveBuffer::<SAMPLE_SIZE>::new()));
+    let SAMPLE_SIZE: usize = (2.0 * 48000.0 / 110.0) as usize;
+
+    let wave_buffer = Arc::new(Mutex::new(WaveBuffer::new(SAMPLE_SIZE)));
 
     let wave_buffer_clone = wave_buffer.clone();
+
+    // Callback closure for audio input stream. It will be called whenever new audio data is available.
     let write_input_data = {
         move |input: &[f32]| {
             // println!("Received input data: {:?}", input.len());
             let mut wb = wave_buffer_clone.lock().unwrap();
-            for &sample in input.iter() {
-                wb.add_samples(&[sample]);
-            }
+            wb.add_samples(input);
         }
     };
 
     let (_opt, stream) = parse_input(write_input_data)?;
+
+    println!(
+        "Starting audio input stream... sample rate: {}, channels: {}",
+        _opt.sample_rate, _opt.channels
+    );
+
     stream.play()?;
 
     App::new()
@@ -65,9 +74,9 @@ fn main() -> Result<(), anyhow::Error> {
                 config: FpsOverlayConfig::default(),
             },
         ))
-        .insert_resource(WaveForm::<SAMPLE_SIZE>(wave_buffer))
+        .insert_resource(WaveForm(wave_buffer))
         .add_systems(Startup, setup)
-        .add_systems(Update, (draw_waveform::<SAMPLE_SIZE>,))
+        .add_systems(Update, (draw_waveform,))
         .run();
     Ok(())
 }
@@ -76,15 +85,15 @@ fn setup(mut commands: Commands) {
     commands.spawn(Camera2d);
 }
 
-fn draw_waveform<const N: usize>(
+fn draw_waveform(
     mut gizmos: Gizmos,
     // _time: Res<Time>,
-    wave_buffer: Res<WaveForm<N>>,
+    wave_buffer: Res<WaveForm>,
 ) {
     let wave_buffer = &*wave_buffer.0.lock().unwrap();
 
     gizmos.linestrip_2d(
-        (0..N).map(|n| {
+        (0..wave_buffer.len).map(|n| {
             let t = n as f32 / wave_buffer.samples.len() as f32;
             let x = (t - 0.5) * 600.0;
             let y = wave_buffer.samples[n] * 200.0;
