56e85bf31814d9670231f9e4b174f07d453b2c9f
Per Lindgren <per.lindgren@ltu.se>
2026-05-18 19:31:48 +0200

    wip

--- README.md
+++ README.md
@@ -12,6 +12,37 @@ For the examples we depend on various crates within the Rust ecosystem.
 
 - [clap](https://crates.io/crates/clap) with `features = "derive".
 
+## Buffer handling
+
+Buffer handling is at heart of all audio applications. For the purpose of audio visualization we adopt a pre-allocated vector, where data is shared between the capture and the widgets, where widgets are presented with read only view underlying data.
+
+![wavebuf](./figs/wavebuf.drawio.svg)
+
+The figure above illustrates the capture and view.
+
+- capture, enqueues incoming audio into the (circular) buffer. This is typically done by the "audio" thread.
+- view, processes the buffered audio.
+
+---
+
+### Tuner application
+
+As an example we develop a (guitar) tuner application.
+
+It is based on an oscilloscope view of the signal, where an in tune signal is steady while an out of tune signal wanders either to the right or to the left. As we will later see, this approach is superior to traditional digital tuners as it provides immediate (zero latency) feedback, with precision beyond the resonance stability of any stringed instrument. Furthermore it allows for tuning against both open and (pinch) harmonics any loss of precision. In fact the taken approach reveals shortcomings of string material, saddles, tuners, and effects of magnetic pull induced by the microphones. Thus besides of tuning, it provides a tool for precise adjustments of intonation and microphone height.
+
+#### Implementation details
+
+Assuming we want to tune for the E2 (the lowest string of a 6-stringed guitar in standard tuning). The frequency of E2 is 82.41Hz with a corresponding period of 1/82.41. Assuming a sample rate of 48000 Hz, the frame length is 582.45358573 (notice we do not round at this point, which will be of importance later).
+
+The wave buffer is spitted akin to a single producer multiple (SPMC) consumer pattern. The consumer provides dual iterators ($i1$, $i2$) over a $period$, where $i1$ and and $i2$ where $i1$ provides the samples from frame $offset$ to start of frame (most recent first), and $i2$ provides the remaining (older) samples of the previous frame (wrapping around if needed). Assume $n$ to be the last sample written by the producer, then:
+
+$$offset = n\%frame$$
+
+This calculation is performed on the floating point representation of the frame length to increase the precision. The view composes the iterator outputs where the last (oldest) sample of $i2$ is followed by the first (most recent sample of $i2$), allowing a continuos line to be formed from the sample points.
+
+For the implementation, we adopt the Rust iterator pattern in a zero copy fashion.
+
 ## License
 
 MIT License
--- src/wave_buffer.rs
+++ src/wave_buffer.rs
@@ -1,90 +1,84 @@
-use std::iter::Iterator;
+use std::iter;
 
-#[derive(Debug)]
-pub struct WaveBuffer {
-    samples: Vec<f32>, // this will be a fixed size buffer
-    in_index: usize,
+struct WaveBuffer {
+    rate: f32,         // sample rate
+    samples: Vec<f32>, // buffer size
+    index: usize,      // next index to write in buffer
 }
 
 impl WaveBuffer {
-    pub fn new(len: usize) -> Self {
+    fn new(rate: f32, len: usize) -> Self {
         Self {
+            rate,
             samples: vec![0.0; len],
-            in_index: 0,
+            index: 0, // we start at index 0.
         }
     }
 
-    fn as_wave_writer(&mut self) -> WaveWriter<'_> {
-        WaveWriter { buffer: self }
-    }
-
-    fn as_wave_reader(&self) -> WaveReader<'_> {
-        WaveReader { buffer: self }
-    }
-
-    pub fn split(&mut self) -> (WaveWriter<'_>, WaveReader<'_>) {
-        let my_self: *mut WaveBuffer = self;
-        let ww = unsafe { &mut *my_self }.as_wave_writer();
-        let wr = unsafe { &*my_self }.as_wave_reader();
-        (ww, wr)
-    }
-}
-
-pub struct WaveWriter<'a> {
-    buffer: &'a mut WaveBuffer,
-}
-
-impl<'a> WaveWriter<'a> {
     #[inline(always)]
     // Wraps around when the buffer is full.
-    pub fn add_samples(&mut self, input: &[f32]) {
+    fn add_samples(&mut self, input: &[f32]) {
         for &sample in input.iter() {
             // if self.index == 0 {
             //     println!(".");
             // }
-            self.buffer.samples[self.buffer.in_index] = sample;
+            self.samples[self.index] = sample;
+            self.index = (self.index + 1) % self.samples.len();
+            if self.index == 0 {
+                println!("Buffer full, wrapping around...");
+            }
+
             //  println!("sample: {:.2} at index {}", sample, self.index);
-            self.buffer.in_index = (self.buffer.in_index + 1) % self.buffer.samples.len();
         }
+    }
+
+    //
+    fn to_iterator(&self, freq: f32) -> impl Iterator<Item = f32> + '_ {
+        let len = self.samples.len();
+        let frame_size = self.rate / freq;
+        let index = self.index;
+        let offset = (self.index as f32 % frame_size) as usize;
+        println!(
+            "self.index {}, index: {}, frame_size: {}",
+            self.index, index, frame_size
+        );
+
+        let i1 = WaveBufferIter {
+            buffer: self,
+            index,
+            nr_items: offset,
+        };
+
+        // i1
+
+        let i2 = WaveBufferIter {
+            buffer: self,
+            index: (self.samples.len() + self.index - offset) % self.samples.len(), // start at the beginning of the frame
+            nr_items: frame_size as usize - offset,
+        };
+
+        i2.chain(i1)
     }
 }
 
-pub struct WaveReader<'a> {
+struct WaveBufferIter<'a> {
     buffer: &'a WaveBuffer,
-}
-
-impl<'a> WaveReader<'a> {
-    #[inline(always)]
-    // Wraps around when the buffer is full.
-    pub fn to_iterator(&'a self, nr_elements: usize) -> WaveReaderIter<'a> {
-        WaveReaderIter {
-            reader: self,
-            index: self.buffer.in_index, // start reading from the current write index
-            nr_elements,
-        }
-    }
-}
-
-pub struct WaveReaderIter<'a> {
-    reader: &'a WaveReader<'a>,
     index: usize,
-    nr_elements: usize,
+    nr_items: usize,
 }
 
-impl<'a> Iterator for WaveReaderIter<'a> {
+impl std::iter::Iterator for WaveBufferIter<'_> {
     type Item = f32;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.nr_elements == 0 {
-            None
-        } else {
-            self.index = (self.index + self.reader.buffer.samples.len() - 1)
-                % self.reader.buffer.samples.len();
-            let sample = self.reader.buffer.samples[self.index];
-
-            self.nr_elements -= 1;
-            Some(sample)
+        if self.nr_items == 0 {
+            return None;
         }
+        let len = self.buffer.samples.len();
+        self.index = (len + self.index - 1) % len; // index points no next, so decrement first 
+        let sample = self.buffer.samples[self.index];
+        self.nr_items -= 1;
+        Some(sample)
     }
 }
 
@@ -94,13 +88,37 @@ mod tests {
 
     #[test]
     fn test_wave_buffer() {
-        let mut buffer = WaveBuffer::new(5);
-        let (mut writer, reader) = buffer.split();
+        let mut buffer = WaveBuffer::new(15.0, 20);
+        for i in 0..25 {
+            buffer.add_samples(&[i as f32]);
+        }
+        println!("Buffer index: {:?}", buffer.index);
+        assert!(buffer.index == 5);
 
-        writer.add_samples(&[1.0, 2.0, 3.0]); // last added to far right
+        let iter = buffer.to_iterator(1.0);
+        for s in iter {
+            println!("Sample: {}", s);
+        }
 
-        let collected: Vec<f32> = reader.to_iterator(3).collect();
-        println!("Collected samples: {:?}", collected);
-        assert_eq!(collected, vec![3.0, 2.0, 1.0]);
+        println!("---");
+        for i in 25..30 {
+            buffer.add_samples(&[i as f32]);
+        }
+        assert!(buffer.index == 10);
+
+        let iter = buffer.to_iterator(1.0);
+        for s in iter {
+            println!("Sample: {}", s);
+        }
+
+        println!("---");
+        for i in 30..35 {
+            buffer.add_samples(&[i as f32]);
+        }
+        assert!(buffer.index == 15);
+        let iter = buffer.to_iterator(1.0);
+        for s in iter {
+            println!("Sample: {}", s);
+        }
     }
 }
